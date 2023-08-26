36931a78e4e7d517eba906fe9cc95ebb91970f08
AMALGSOFT <rivangushu200241>
2023-08-26 16:32:16 +0300

    second commt2

--- server/config/express.js
+++ server/config/express.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser');
 // const auth = require('../middlewares/auth');
 const cors = require('cors');
 module.exports = (app) => {
-    app.use(cors({origin: 'https://quickie-ztoq.vercel.app', credentials: true}));
+    app.use(cors({origin: 'http://quickie-ztoq.vercel.app', credentials: true}));
     app.use(express.json({ limit: '50mb' }));
     app.use(express.urlencoded({ limit: '50mb', extended: true }));
     app.use(cookieParser());
