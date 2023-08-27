6ad3a19065c4a9309a485c210d8ee8cf1bd8b73c
AMALGSOFT <rivangushu200241>
2023-08-27 18:03:47 +0300

    second commt2

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ const TABLE_NAME = 'all-for-you';
 const config = {
     PORT: process.env.PORT || 5000,
     //DB_CONNECTION: `mongodb://localhost/${TABLE_NAME}`,
-    DB_CONNECTION: `mongodb+srv://2100813768:C4BRX87h8bAK36tm@all-for-you.kijyq33.mongodb.net/all-for-you?retryWrites=true&w=majority`,
+    DB_CONNECTION: `mongodb+srv://2100813768:C4BRX87h8bAK36tm@all-for-you.kijyq33.mongodb.net/quickie?retryWrites=true&w=majority`,
     SECRET: 'secret',
     SALT: 10,
     COOKIE_NAME: 'USER_SESSION',
